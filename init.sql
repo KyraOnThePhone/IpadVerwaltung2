12f05ef512390931e4a018c3d4f72addb32bffde
kyra <kymitw001@binf02p352.dmawi.de>
2024-09-26 12:46:51 +0200

    alles mögliche keine ahnung mehr

--- init.sql
+++ init.sql
@@ -1,1 +1,12 @@
-create database [IPadVerwaltung];
+CREATE database IPadVerwaltung;
+
+CREATE database Login;
+
+CREATE TABLE Accounts (
+    id INT IDENTITY PRIMARY KEY NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+INSERT INTO Accounts (username, password)
+VALUES ('IN23','$2y$10$MWYm7RKEVJ8Us7S1S4j/n.l4yEDQzytDMH15PCFT0YZvYGc7nqUnC');
---
+++ webserver_data/login.php
@@ -0,0 +1,3 @@
+<?php
+echo password_hash('IN23GegenFacharbeiten!', PASSWORD_DEFAULT);
+echo 'Hello World';
