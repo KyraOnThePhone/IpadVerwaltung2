9fc83641546a4ccea720c3ced003ecad30fde7ed
kyra <kymitw001@binf02p352.dmawi.de>
2024-10-28 10:33:15 +0100

    dockerfile, init works

--- init.sql
+++ init.sql
@@ -1,12 +1,26 @@
-CREATE database IPadVerwaltung;
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'IPadVerwaltung')
+BEGIN
+    CREATE DATABASE IPadVerwaltung;
+END
 
-CREATE database Login;
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'Login')
+BEGIN
+    CREATE DATABASE Login;
+END
 
-CREATE TABLE Accounts (
-    id INT IDENTITY PRIMARY KEY NOT NULL,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
+-- Wechseln zur Datenbank IPadVerwaltung
+USE IPadVerwaltung;
 
+-- Erstellen der Tabelle Accounts, falls sie nicht existiert
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE name = 'Accounts' AND type = 'U')
+BEGIN
+    CREATE TABLE Accounts (
+        id INT IDENTITY PRIMARY KEY NOT NULL,
+        username VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL
+    );
+END
+
+-- Einfügen eines Benutzers in die Accounts-Tabelle
 INSERT INTO Accounts (username, password)
 VALUES ('IN23','$2y$10$MWYm7RKEVJ8Us7S1S4j/n.l4yEDQzytDMH15PCFT0YZvYGc7nqUnC');
