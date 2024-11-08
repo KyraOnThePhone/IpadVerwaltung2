b0e5e7bedd97db668a80e3d4d5b9b526a36619a8
kyra <kymitw001@binf02p352.dmawi.de>
2024-11-08 10:51:38 +0100

    sqlgo

--- init.sql
+++ init.sql
@@ -2,13 +2,15 @@ IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'IPadVerwaltung')
 BEGIN
     CREATE DATABASE IPadVerwaltung;
 END
+GO
 
 IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'Login')
 BEGIN
     CREATE DATABASE Login;
 END
-
+GO
 USE Login;
+GO
 
 
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE name = 'Accounts' AND type = 'U')
@@ -19,6 +21,7 @@ BEGIN
         password VARCHAR(255) NOT NULL
     );
 END
+GO
 
 
 IF NOT EXISTS (SELECT * FROM Accounts WHERE username= 'IN23')
@@ -26,5 +29,6 @@ BEGIN
 INSERT INTO Accounts (username, password)
 VALUES ('IN23','$2y$10$MWYm7RKEVJ8Us7S1S4j/n.l4yEDQzytDMH15PCFT0YZvYGc7nqUnC');
 END
+GO
 
 USE IPadVerwaltung;
